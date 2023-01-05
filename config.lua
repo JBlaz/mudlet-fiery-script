8c35b8c4121a1505d1beccc37c7bff89cc0d460b
Jase <jase.bleazard@orbitonline.com>
2023-01-04 18:28:46 -0700

    Extracted file

---
+++ config.lua
@@ -0,0 +1,6 @@
+mpackage = [[Blaz]]
+author = [[Zaver]]
+title = [[FieryMUD package for Multi-Players.]]
+description = [[This package has several features including auto spelling, healing, and skills. These features should ONLY BE USED TO AID YOU WHILE PLAYING FieryMUD does not allow botting! If you have issues or would like a feature added send an email to blazspams@gmail.com]]
+version = [[0.0.1]]
+created = "2022-12-25T21:09:16-07:00"
